03aa3e3dffb2e8964e7b8b629bec1102be304aea
alexfias <kies@fias.uni-frankfurt.de>
2023-03-28 11:05:48 +0200

    Update environment.py

--- environment.py
+++ environment.py
@@ -1,26 +1,21 @@
 import gym
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten
-from keras.optimizers import Adam
-from rl.agents.dqn import DQNAgent
-from rl.policy import EpsGreedyQPolicy
-from rl.memory import SequentialMemory
 
 class EnergyArbitrageEnv(gym.Env):
-    def __init__(self):
+    def __init__(self, storage_capacity=10):
         self.action_space = gym.spaces.Discrete(3)
         self.observation_space = gym.spaces.Box(low=0, high=np.inf, shape=(13,), dtype=np.float32)
         self.energy_prices = np.array([])
         self.current_step = 0
         self.storage_level = 0
+        self.storage_capacity = storage_capacity
 
     def set_energy_prices(self, prices):
         self.energy_prices = prices
 
     def step(self, action):
         if action == 0:  # Buy
-            self.storage_level += 1
+            self.storage_level = min(self.storage_level + 1, self.storage_capacity)
         elif action == 1:  # Sell
             self.storage_level = max(0, self.storage_level - 1)
 
