b457ac01055dba6002a1de9cfd81779ad44a098b
Alexander Kies <kies@fias.uni-frankfurt.de>
2023-04-05 23:02:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,71 @@
-from environment import EnergyArbitrageEnv
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten
-from keras.optimizers import Adam
-from rl.agents.dqn import DQNAgent
-from rl.policy import EpsGreedyQPolicy
-from rl.memory import SequentialMemory
+import numpy as np
+import tensorflow as tf
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.drivers import dynamic_step_driver
+from tf_agents.environments import tf_py_environment
+from tf_agents.networks import q_network
+from tf_agents.policies import random_tf_policy
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.trajectories import trajectory
+from tf_agents.utils import common
 
-#load energy price data
-energy_prices = np.array([...])
+from environment import EnergyArbitrageEnv
+
+# Replace with your own energy price data
+energy_prices = np.array([10, 20, 15, 12, 8, 5, 30, 25, 18, 22, 11, 14, 9, 6, 31, 26,10, 20, 15, 12, 8, 5, 30, 25, 18, 22, 11, 14, 9, 6, 31, 26])
 
 storage_capacity = 10
 env = EnergyArbitrageEnv(storage_capacity=storage_capacity)
 env.set_energy_prices(energy_prices)
 
-hidden_units = 32
-action_size = env.action_space.n
-memory_size = 10000
-nb_steps_warmup = 1000
-target_model_update = 1e-2
+train_py_env = EnergyArbitrageEnv(storage_capacity=storage_capacity)
+train_py_env.set_energy_prices(energy_prices)
+eval_py_env = EnergyArbitrageEnv(storage_capacity=storage_capacity)
+eval_py_env.set_energy_prices(energy_prices)
 
-model = Sequential()
-model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
-model.add(Dense(hidden_units))
-model.add(Activation('relu'))
-model.add(Dense(hidden_units))
-model.add(Activation('relu'))
-model.add(Dense(action_size))
-model.add(Activation('linear'))
+train_env = tf_py_environment.TFPyEnvironment(train_py_env)
+eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
 
-memory = SequentialMemory(limit=memory_size, window_length=1)
-policy = EpsGreedyQPolicy()
-dqn = DQNAgent(model=model, nb_actions=action_size, memory=memory, nb_steps_warmup=nb_steps_warmup,
-               target_model_update=target_model_update, policy=policy)
-dqn.compile(Adam(lr=0.001), metrics=['mae'])
+fc_layer_params = (32, 32)
+q_net = q_network.QNetwork(
+    train_env.observation_spec(),
+    train_env.action_spec(),
+    fc_layer_params=fc_layer_params)
 
-#train and test the agent
-dqn.fit(env, nb_steps=5000, visualize=False, verbose=1)
+optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=0.001)
 
-dqn.test(env, nb_episodes=100, visualize=False)
+global_step = tf.compat.v1.train.get_or_create_global_step()
+
+agent = dqn_agent.DqnAgent(
+    train_env.time_step_spec(),
+    train_env.action_spec(),
+    q_network=q_net,
+    optimizer=optimizer,
+    td_errors_loss_fn=common.element_wise_squared_loss,
+    train_step_counter=global_step)
+agent.initialize()
+
+replay_buffer_capacity = 10000
+
+replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+    data_spec=agent.collect_data_spec,
+    batch_size=train_env.batch_size,
+    max_length=replay_buffer_capacity)
+
+num_episodes = 100
+
+for episode in range(num_episodes):
+    time_step = train_env.reset()
+    episode_return = 0.0
+
+    while not time_step.is_last():
+        action_step = agent.collect_policy.action(time_step)
+        next_time_step = train_env.step(action_step.action)
+        traj = trajectory.from_transition(time_step, action_step, next_time_step)
+
+        replay_buffer.add_batch(traj)
+
+        episode_return += time_step.reward
+        time_step = next_time_step
+
+    print(f"Episode {episode}: {episode_return.numpy()}")
