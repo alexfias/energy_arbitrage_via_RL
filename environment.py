9a1b3e5af5f59a94fd6813298807a60e52b82387
Alexander Kies <kies@fias.uni-frankfurt.de>
2023-04-05 23:28:34 +0200

    Update environment.py

--- environment.py
+++ environment.py
@@ -1,60 +1,50 @@
 import numpy as np
-import gym
-from gym import spaces
 from tf_agents.environments import py_environment
 from tf_agents.specs import array_spec
 from tf_agents.trajectories import time_step as ts
 
-class EnergyArbitrageEnv(py_environment.PyEnvironment):
-
-    def __init__(self, storage_capacity=10):
-        super().__init__()
-        self.storage_capacity = storage_capacity
-        self.action_space = spaces.Discrete(3)  # 0: Buy, 1: Sell, 2: Hold
-        self.observation_space = spaces.Box(low=0, high=np.inf, shape=(4,), dtype=np.float32)
-        self.energy_prices = np.array([])
-        self.reset()
-
-    def set_energy_prices(self, energy_prices):
+class EnergyArbitrageEnvironment(py_environment.PyEnvironment):
+    def __init__(self, energy_prices, storage_capacity, storage_level, storage_efficiency):
         self.energy_prices = energy_prices
+        self.storage_capacity = storage_capacity
+        self.storage_level = storage_level
+        self.storage_efficiency = storage_efficiency
+        self.current_step = 0
+
+        self._action_spec = array_spec.BoundedArraySpec(
+            shape=(), dtype=np.int32, minimum=0, maximum=2, name='action')
+        
+        self._observation_spec = array_spec.BoundedArraySpec(
+            shape=(4,), dtype=np.float32, minimum=[0, 0, 0, 0], maximum=[np.finfo(np.float32).max, np.finfo(np.float32).max, len(energy_prices)-1, storage_capacity], name='observation')
+
+    def action_spec(self):
+        return self._action_spec
+
+    def observation_spec(self):
+        return self._observation_spec
 
     def _reset(self):
         self.current_step = 0
         self.storage_level = 0
-        return ts.restart(self._get_observation())
+        return ts.restart(np.array([self.energy_prices[0], self.energy_prices[1], 0, self.storage_level], dtype=np.float32))
 
     def _step(self, action):
-        if self.current_step == len(self.energy_prices) - 1:
-            return ts.termination(self._get_observation(), 0)
-        
+        if self.current_step == len(self.energy_prices) - 2:
+            return self.reset()
+
         reward = 0
         price = self.energy_prices[self.current_step]
-        next_price = self.energy_prices[self.current_step + 1] if self.current_step < len(self.energy_prices) - 2 else price
-        
+        next_price = self.energy_prices[self.current_step + 1]
+
         if action == 0:  # Buy
-            reward = next_price - price
-            self.storage_level = min(self.storage_level + 1, self.storage_capacity)
+            energy_to_buy = min(self.storage_capacity - self.storage_level, self.storage_level / price)
+            self.storage_level += energy_to_buy * self.storage_efficiency
+            reward = -energy_to_buy * price
         elif action == 1:  # Sell
-            reward = price - next_price
-            self.storage_level = max(self.storage_level - 1, 0)
-        elif action == 2:  # Hold
-            reward = 0
+            energy_to_sell = min(self.storage_level, self.storage_level * price)
+            self.storage_level -= energy_to_sell
+            reward = energy_to_sell * next_price
 
         self.current_step += 1
-
-        return ts.transition(self._get_observation(), reward, discount=1.0)
-
-    def _get_observation(self):
-        if len(self.energy_prices) < 2:
-            return np.zeros(4, dtype=np.float32)
-        next_price = self.energy_prices[self.current_step + 1] if self.current_step < len(self.energy_prices) - 2 else self.energy_prices[self.current_step]
-        return np.array([self.energy_prices[self.current_step], next_price, self.current_step, self.storage_level], dtype=np.float32)
-
-    def action_spec(self):
-        return array_spec.BoundedArraySpec(shape=(), dtype=np.int32, minimum=0, maximum=2, name='action')
-
-    def observation_spec(self):
-        return array_spec.BoundedArraySpec(shape=(4,), dtype=np.float32, minimum=[0, 0, 0, 0], maximum=[np.inf, np.inf, np.inf, self.storage_capacity], name='observation')
-
-    def _render(self, mode='human', close=False):
-        pass
+        next_observation = np.array([self.energy_prices[self.current_step], self.energy_prices[self.current_step + 1], self.current_step, self.storage_level], dtype=np.float32)
+        return ts.transition(next_observation, reward)
